d846d44ad060c07cc9a22755193025e7ea8ab4dc
vishal1145 <vishal.kumar1145@gmail.com>
2026-02-24 13:03:24 +0530

    fix chat id

--- backend/config.py
+++ backend/config.py
@@ -7,6 +7,7 @@ SMTP_FROM_EMAIL = "coder.rajat07@gmail.com"
 
 # Telegram Configuration
 TELEGRAM_BOT_TOKEN = "8653643537:AAH4kaIH-mEQIB_hZ-FWPuM3B-eyUWrtYsc"
+TELEGRAM_CHAT_ID = "5770045910"
 
 # Server Configuration
 BASE_URL = "https://recruitpulse.algofolks.com"
