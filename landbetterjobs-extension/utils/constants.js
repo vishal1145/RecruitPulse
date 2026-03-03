82893ce97499254cbfe7176dbab2fb991825ccfe
vishal1145 <vishal.kumar1145@gmail.com>
2026-03-03 13:54:56 +0530

    delay time reduce

--- landbetterjobs-extension/utils/constants.js
+++ landbetterjobs-extension/utils/constants.js
@@ -9,7 +9,7 @@ export const API_ENDPOINT = `${API_BASE_URL}/api/jobs`;
 export const API_TEST_RESET_URL = `${API_BASE_URL}/api/test/reset-jobs`;
 
 // Milliseconds to wait between processing each job
-export const JOB_DELAY_MS = 60000; // 1 minute
+export const JOB_DELAY_MS = 30000; // 1 minute
 
 // Retry configuration
 export const MAX_RETRIES = 3;
